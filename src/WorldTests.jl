376d7dc71cf4c7f08b7827dbc5bc0c846e285ed3
Jack Beasley <jack.beasley@gmail.com>
2019-12-05 22:16:32 -0800

    Remove old world code

--- src/WorldTests.jl
+++ src/WorldTests.jl
@@ -5,35 +5,4 @@ using ..Individuals
 
 using LightGraphs, Distributions
 
-struct TestSettings
-    g::LightGraphs.AbstractGraph
-    trialsPerStep::Integer
-    actionPrbs::Vector{Float64}
-    alphaDist::Distributions.Distribution
-    betaDist::Distributions.Distribution
-    numTests::Integer
-end
-
-function run_trial_for_world(world::World, iterations::Integer)
-    for _ in 1:iterations
-        world  = step_world(world)
-    end
-
-    resulting_actions = [select_action(indiv) for indiv in world.individuals]
-
-    return all(elem -> elem == argmax(world.actionProbabilities), resulting_actions)
-end
-
-function test_world(s::TestSettings)
-
-    numSuccess = 0
-    for _ in 1:s.numTests
-        if run_trial_for_world(World(s.g, s.trialsPerStep, s.actionPrbs, s.alphaDist, s.betaDist), 10000)
-            numSuccess += 1
-        end
-    end
-    return (numSuccess / s.numTests)
-end
-
-export TestSettings, run_trial_for_world, test_world
 end
--- src/Worlds.jl
+++ src/Worlds.jl
@@ -118,64 +118,6 @@ function update_with_observations(indiv::BetaIndividual, observations::TotalObse
     return BetaIndividual(indiv.id, new_alpha_values, new_beta_values)
 end
 
-struct World
-    structure::LightGraphs.AbstractGraph
-    individuals::Vector{BetaIndividual}
-    actionProbabilities::Vector{Real}
-    actionDistributions::Vector{Distribution}
-    trialsPerStep::Integer
-end
-
-function World(g::LightGraphs.AbstractGraph,
-               trialsPerStep::Integer,
-               actionPrbs::Vector{Float64},
-               alphaDist::Distributions.Distribution, betaDist::Distributions.Distribution)
-    individuals = [BetaIndividual(i, size(actionPrbs)[1], alphaDist, betaDist) for i in 1:nv(g)]
-    actionDists = [
-    Binomial(trialsPerStep, actionPrb) for actionPrb in actionPrbs]
-    return World(g, individuals, actionPrbs, actionDists, trialsPerStep)
-end
-
-function run_trials(w::World)::Tuple{Matrix{Int64}, Matrix{Int64}}
-    shape = (length(w.individuals), length(w.actionProbabilities))
-    numSuccesses = zeros(Int64, shape)
-    numTrials = zeros(Int64, shape)
-
-    for indivID in 1:length(w.individuals)
-        actionID = select_action(w.individuals[indivID])
-
-        numSuccesses[indivID, actionID] = rand(w.actionDistributions[actionID])
-        numTrials[indivID, actionID] = w.trialsPerStep
-    end
-
-    return (numSuccesses, numTrials)
-end
-
-function step_individual(w::World, indiv::BetaIndividual, numSuccesses, numTrials)
-    neighbors = [indiv.id, outneighbors(w.structure, indiv.id)...]
-
-    successesByAction = zeros(Int64, length(w.actionProbabilities))
-    trialsByAction = zeros(Int64, length(w.actionProbabilities))
-
-    for neighborID in neighbors
-        for actionID in 1:length(w.actionProbabilities)
-            successesByAction[actionID] += numSuccesses[neighborID, actionID]
-            trialsByAction[actionID] += numTrials[neighborID, actionID]
-        end
-    end
-    return update_with_results(indiv, successesByAction, trialsByAction)
-end
-
-function step_world(w::World)::World
-    (numSuccesses, numTrials) = run_trials(w)
-
-    new_individuals = [step_individual(w, indiv, numSuccesses, numTrials) for indiv in w.individuals]
-
-    return World(w.structure, new_individuals, w.actionProbabilities,
-        w.actionDistributions, w.trialsPerStep)
-
-end
-
-export World, run_trials, step_world, ZollmanModelState, step_model
+export ZollmanModelState, step_model
 
 end
